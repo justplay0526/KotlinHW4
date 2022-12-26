34e244b52f93197c906a00aa40d3c3d6bf050ded
黃彥崴 <justplay0526@gmail.com>
2022-12-26 12:47:19 +0800

    first commit

---
+++ Ch13/app/src/main/java/com/example/lab13_boardcastreceiver/MyService.kt
@@ -0,0 +1,50 @@
+package com.example.lab13_boardcastreceiver
+
+import android.app.Service
+import android.content.Intent
+import android.os.Bundle
+import android.os.IBinder
+
+class MyService : Service() {
+    private var h = 0
+    private var m = 0
+    private var s = 0
+    override fun onStartCommand(intent: Intent, flags: Int, startID: Int): Int {
+        flag = intent.getBooleanExtra("flag", false)
+        Thread {
+            while (flag) {
+                try {
+                    Thread.sleep(10)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                s++
+                if (s >= 60) {
+                    s = 0
+                    m++
+                    if (m >= 60) {
+                        m = 0
+                        h++
+                    }
+                }
+                val i = Intent("MyMessage")
+                val bundle = Bundle()
+                bundle.putInt("H", h)
+                bundle.putInt("M", m)
+                bundle.putInt("S", s)
+                i.putExtras(bundle)
+                sendBroadcast(i)
+            }
+        }.start()
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        // TODO: Return the communication channel to the service.
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    companion object {
+        var flag = false
+    }
+}
